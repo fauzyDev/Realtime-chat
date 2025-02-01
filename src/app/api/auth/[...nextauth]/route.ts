c63961c686402b56f69c274ddf2834c20bce92b6
fauzyDev <haliffauzy45@gmail.com>
2025-02-01 13:59:54 +0700

    code update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,8 @@ import Google from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth"
 import { SupabaseAdapter } from "@auth/supabase-adapter"
 
-const clientId: string = process.env.GOOGLE_ID ?? "";
-const clientSecret: string = process.env.GOOGLE_SECRET ?? "";
+const clientId: string = process.env.SECRET_GOOGLE_CLIENT_ID ?? "";
+const clientSecret: string = process.env.SECRET_GOOGLE_SECRET_ID ?? "";
 const url: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const secret: string = process.env.SUPABASE_SERVICE_ROLE_KEY ?? "";
 
@@ -19,7 +19,8 @@ export const authOptions: NextAuthOptions = {
         url,
         secret
     }),
-    secret: process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
+    session: { strategy: "database" }
 }
 
 const handler = NextAuth(authOptions)
