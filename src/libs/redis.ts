53605ea5cbe6e1a31245e1d388898ba86c97698c
fauzyDev <haliffauzy45@gmail.com>
2025-03-08 20:02:52 +0700

    bug code

--- src/libs/redis.ts
+++ src/libs/redis.ts
@@ -1,7 +1,3 @@
 import { Redis } from '@upstash/redis'
-// import "dotenv/config";
 
-export const redis = new Redis({
-    url: "https://liberal-buffalo-42570.upstash.io",
-    token: "AaZKAAIjcDE3NjBiM2E4NmMwYzc0MGJlYWRlZmM0NmIyNGJiMjc4MnAxMA"
-})
+export const redis = Redis.fromEnv();
